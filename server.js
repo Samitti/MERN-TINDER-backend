7ec237567d6a333b82c6cb51f86f955c58c886a0
Samitti <samuelteweldebrhan@gmail.com>
2021-12-24 12:32:26 +0300

    add post and get end-points

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import Cards from './dbCards.js';
+
 // App Config
 const app = express();
 const port = process.env.PORT || 8001;
@@ -8,10 +10,33 @@ const connection_url = 'mongodb+srv://admin:ExxQO503vaNSh5Id@cluster0.duyfm.mong
 
 // Midlewares
 
-// DB config 
+// DB config
+mongoose.connect(connection_url);
 
 // API Endpoints 
 app.get('/', (req, res) => res.status(200).send('HELLO SAMI!!'));
+
+app.post('/tinder/cards', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    })
+});
+
+app.get('/tinder/cards', (req, res) => {    
+    Cards.find((err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    })
+});
 
 // Listener
 app.listen(port, () => console.log(`Listening on port: ${port}`));
